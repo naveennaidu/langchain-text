b8f5070db40beb5cc4ff304920df47045938255a
Naveen <mnaveennaidu9927@gmail.com>
2023-04-22 17:03:33 +0800

    added ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineNuxtConfig({
+  ssr: false,
   runtimeConfig: {
     public: {
       openAiKey: process.env.OPEN_AI_KEY,
